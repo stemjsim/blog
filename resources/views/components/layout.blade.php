44b14fefb327723d0ef7c2d5872925ce69ec2c43
S Simpson <B00418234@studentmail.uws.ac.uk>
2022-12-08 17:12:23 +0000

    styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -101,7 +101,7 @@
 
     @if (session()->has('success'))
         <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 10000)" x-show="show"
-            class="fixed bottom-3 right-3 bg-blue-500 text-white py-2 px-4 rounded-xl">
+            class="fixed bottom-3 right-3 bg-green-500 text-white py-2 px-4 rounded-xl">
             <p>
                 {{ session('success') }}
             </p>
