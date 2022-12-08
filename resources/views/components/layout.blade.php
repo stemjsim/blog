77c1d1ab17c9ad75327a84f4abcdbf238872bb65
S Simpson <B00418234@studentmail.uws.ac.uk>
2022-12-08 16:52:38 +0000

    increase length of success message

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -100,7 +100,7 @@
     </section>
 
     @if (session()->has('success'))
-        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 5000)" x-show="show"
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 10000)" x-show="show"
             class="fixed bottom-3 right-3 bg-blue-500 text-white py-2 px-4 rounded-xl">
             <p>
                 {{ session('success') }}
