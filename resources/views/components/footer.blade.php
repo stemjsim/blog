bd270f57531d9fe40bf76bdc2106a821e7ad823d
S Simpson <B00418234@studentmail.uws.ac.uk>
2022-12-11 16:37:55 +0000

    tidy up

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
-    <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+    <img src="/images/newsletter.svg" alt="" class="mx-auto mb-6" style="width: 145px;">
     <h5 class="text-3xl">Hear what we have to say twice a week</h5>
     <p class="text-sm mt-3">Helpful insights right into your inbox</p>
 
